a522af0dc85ae9a4c6cabfa4f9b9af26222e7c7d
Zak <zsanderford@gmail.com>
2017-02-04 16:25:25 -0600

    Munchkin Game Master Web Application

---
+++ app/app.js
@@ -0,0 +1,71 @@
+var munchkinGM = angular.module('munchkinGM', ['ngRoute', 'ngAnimate']);
+
+munchkinGM.config(['$routeProvider', /*'$locationProvider',*/ function($routeProvider/*, $locationProvider*/){
+
+  //$locationProvider.html5Mode(true);
+
+  $routeProvider
+    .when('/home',{
+      templateUrl:'views/home.html',
+      controller: ''
+    })
+    .when('/pregame',{
+      templateUrl:'views/pregame.html',
+      controller: ''
+    })
+    .when('/ingame', {
+        templateUrl: 'views/ingame.html',
+        controller: ''
+    })
+    .when('/endgame', {
+        templateUrl: 'views/endgame.html',
+        controller: ''
+    })
+    .when('/contact', {
+      templateUrl: 'views/contact.html',
+      controller: ''
+    })
+    .when('/contact-success', {
+      templateUrl: 'views/contact-success.html',
+      controller: ''
+    })
+    .otherwise({
+    redirectTo: '/home'
+    });
+
+}]);
+
+munchkinGM.controller('PlayerController', ['$scope', '$http', function($scope, $http){
+
+    $scope.removePlayer = function(player){
+      var removedPlayer = $scope.players.indexOf(player);
+      $scope.players.splice(removedPlayer, 1);
+    }
+
+    $scope.addPlayer = function () {
+      $scope.players.push({
+        name: $scope.newPlayer.name,
+        gender: $scope.newPlayer.gender
+      });
+      $scope.newPlayer.name = "";
+      $scope.newPlayer.gender = "";
+    }
+
+    $scope.removeAll = function(){
+        $scope.players = [];
+    };
+
+    $http.get('data/players.json').then(function(playerData){
+      $scope.players = playerData.data;
+    });
+
+
+}]);
+
+munchkinGM.controller('ContactController', ['$scope', '$location', function($scope, $location){
+
+  $scope.sendMessage = function(){
+    $location.path('/contact-success')
+  };
+
+}]);
